f28657669583506968f3fb6ea0fe6f6a01302940
oct2i <oct2i@yandex.ru>
2019-04-14 12:40:08 +0700

    add migration, add InlineModelAdmin for test questions

--- service/admin.py
+++ service/admin.py
@@ -7,6 +7,11 @@ admin.site.register(Candidate)
 admin.site.register(Jedi)
 admin.site.register(Planet)
 admin.site.register(Orden)
-admin.site.register(Test)
-admin.site.register(Question)
 admin.site.register(Answer)
+
+class QuestionInstanceInline(admin.TabularInline):
+    model = Question
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    inlines = [QuestionInstanceInline]
